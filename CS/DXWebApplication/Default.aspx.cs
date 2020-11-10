e83a1fc806c3c3b8a2d9106b3e5472ae748eba0d
natashaash <natalya.shirokopoyas@devexpress.com>
2020-11-10 21:21:56 +0300

    add example content

---
+++ CS/DXWebApplication/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DXWebApplication14 {
+    public partial class WebForm1 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+        protected void ASPxFormLayout1_E3_Init(object sender, EventArgs e) {
+            ASPxTokenBox tb = sender as ASPxTokenBox;
+            tb.DataSource = GanttDataProvider.GetResources();
+            tb.DataBindItems();
+        }
+
+       
+    
+    }
+}
