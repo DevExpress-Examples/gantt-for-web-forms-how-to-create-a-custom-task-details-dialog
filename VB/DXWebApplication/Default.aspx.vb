43ddc9fb36175c02b83242636f4d3b57a84caf42
DevExpressExampleBot <fakeEmail>
2021-12-24 17:34:54 +0300

    Source auto update [skip ci]

--- VB/DXWebApplication/Default.aspx.vb
+++ VB/DXWebApplication/Default.aspx.vb
@@ -1,25 +1,20 @@
-﻿Imports DevExpress.Web
+Imports DevExpress.Web
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Web
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
 
 Namespace DXWebApplication14
-	Partial Public Class WebForm1
-		Inherits System.Web.UI.Page
 
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+    Public Partial Class WebForm1
+        Inherits Page
 
-		End Sub
-		Protected Sub ASPxFormLayout1_E3_Init(ByVal sender As Object, ByVal e As EventArgs)
-			Dim tb As ASPxTokenBox = TryCast(sender, ASPxTokenBox)
-			tb.DataSource = GanttDataProvider.GetResources()
-			tb.DataBindItems()
-		End Sub
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        End Sub
 
-
-
-	End Class
+        Protected Sub ASPxFormLayout1_E3_Init(ByVal sender As Object, ByVal e As EventArgs)
+            Dim tb As ASPxTokenBox = TryCast(sender, ASPxTokenBox)
+            tb.DataSource = GetResources()
+            tb.DataBindItems()
+        End Sub
+    End Class
 End Namespace
